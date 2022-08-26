03c30b7fe192cba82a6d03a5634f5bafd0519603
Jun Earl Solomon <junearlsolomon14@gmail.com>
2022-08-26 02:37:54 -0700

    updated dependencies, added spot class

---
+++ app/src/main/java/com/example/dater/Spot.java
@@ -0,0 +1,8 @@
+package com.example.dater;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.google.android.gms.maps.model.LatLng;
+//public class Spot implements ClusterItem {
+//}
